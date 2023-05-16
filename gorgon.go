97344eddd055ea19ad0dfd037d95f03cc10c07d9
Nicholas Lee <nicholasleexyz@gmail.com>
2023-05-16 16:13:44 -0600

    git ignore,  coords

--- gorgon.go
+++ gorgon.go
@@ -4,12 +4,30 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"math/rand"
 	"os"
+	"time"
 )
+
+func randRange(min int, max int) int {
+	return rand.Intn(max-min+1) + min
+}
+
+type Coord struct {
+	x int
+	y int
+}
+
+func returnCoord(x int, y int) Coord {
+	return Coord{x, y}
+}
 
 func main() {
 	width := 512
 	height := 512
+
+	column := 16
+	row := 16
 
 	upLeft := image.Point{0, 0}
 	lowRight := image.Point{width, height}
@@ -19,11 +37,28 @@ func main() {
 	// Colors are defined by Red, Green, Blue, Alpha uint8 values.
 	cyan := color.RGBA{100, 200, 200, 0xff}
 
+	rand.Seed(time.Now().UnixNano())
+
 	// Set color for each pixel.
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
-			nx := (x * 4) / width
-			ny := (y * 4) / height
+
+			/*
+
+				pick n amount of random coordinates
+				for each pixel find the nearest of the random coords
+
+				profit?! :P
+
+				random number between a range
+				calculate the distance between two 2-dimensional coordinates
+
+			*/
+
+			//normalized x and y could also be used with this
+
+			nx := (x * column) / width
+			ny := (y * row) / height
 
 			if nx%2 == ny%2 {
 				img.Set(x, y, cyan)
