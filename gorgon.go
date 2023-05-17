df218b7e12f7a476225f4f2958fb085a4f9723a3
Nicholas Lee <nicholasleexyz@gmail.com>
2023-05-16 21:15:07 -0600

    distance from cell

--- gorgon.go
+++ gorgon.go
@@ -4,18 +4,30 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"math"
 	"math/rand"
 	"os"
 	"time"
 )
 
-type Coord struct {
-	x int
-	y int
+type Cell struct {
+	x     int
+	y     int
+	color color.RGBA
 }
 
 func randrange(min int, max int) int {
 	return rand.Intn(max-min+1) + min
+}
+
+func distancefromcell(x int, y int, cell Cell) float64 {
+	//d=√((x2 – x1)² + (y2 – y1)²) // formula for distance
+	x1 := float64(x)
+	y1 := float64(y)
+	x2 := float64(cell.x)
+	y2 := float64(cell.y)
+
+	return math.Sqrt(math.Pow((x2-x1), 2) + math.Pow((y2-y1), 2))
 }
 
 func main() {
@@ -32,22 +44,28 @@ func main() {
 
 	// Colors are defined by Red, Green, Blue, Alpha uint8 values.
 	cyan := color.RGBA{100, 200, 200, 0xff}
-
+	grey := color.RGBA{127, 127, 127, 0xff}
 	rand.Seed(time.Now().UnixNano())
 
 	numcrds := 8
-	crds := []Coord{}
+	crds := []Cell{}
 
 	for i := 0; i < numcrds; i++ {
-		crd := Coord{randrange(0, column-1), randrange(0, row-1)}
+		col := cyan
+		if i%2 == 0 {
+			col = color.RGBA{255, 255, 255, 0xff}
+		}
+
+		crd := Cell{randrange(0, column-1), randrange(0, row-1), col}
 		crds = append(crds, crd)
 	}
 
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
-			img.Set(x, y, cyan)
+			img.Set(x, y, grey)
 		}
 	}
+
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
 			/*
@@ -65,7 +83,7 @@ func main() {
 			for i := 0; i < numcrds; i++ {
 				crd := crds[i]
 				if nx == crd.x && ny == crd.y {
-					img.Set(x, y, color.White)
+					img.Set(x, y, crd.color)
 				}
 			}
 		}
